a5ea73ba4534ede30940c8a2e47035a093f07db8
Gounaya <o.gounaya@gmail.com>
2018-05-31 00:03:22 +0200

    add app js

--- app.js
+++ app.js
@@ -1,35 +1,12 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var io = require('socket.io').listen(server);
 
+server.listen(3000);
 
 app.get('/', function(req, res){
-  if(ioController.isFull()){
-    res.sendFile(__dirname + "/full.html");
-  }else{
     res.sendFile(__dirname + "/index.html");
-  }
 });
 
-app.get('*', function(req, res){
-  res.sendFile(__dirname + "/error.html");
-})
 
-io.on('connection', function (socket){
-
-    ioController.connect();
-
-  socket.on('disconnect', function(){
-    ioController.handleDisconnect();
-  });
-
-  socket.on('chat message', function(msg){
-    ioController.handleMessage(msg);
-  });
-
-});
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
